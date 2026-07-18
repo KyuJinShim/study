d2050579d5a9d1b4f150ae10f4ce59bce16fd71c
KyuJinShim <tlarbwls95@naver.com>
2026-07-18 14:13:05 +0900

    [Passed] 벽이 없는 충돌 실험 (2ms, 0MB)

---
+++ trail4/벽이 없는 충돌 실험/collision-experiment-without-wall.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int T, N;
+struct ball {
+    int num;
+    int x;
+    int y;
+    int w;
+    char d;
+};
+
+struct Compare_pos {
+    bool operator()(const ball& a, const ball& b)
+    {
+        if (a.y == b.y)
+        {
+            return a.x > b.x;
+        }
+        return a.y > b.y;
+    }
+};
+
+struct Compare_power {
+    bool operator()(const ball& a, const ball& b)
+    {
+        if (a.w == b.w)
+        {
+            return a.num > b.num;
+        }
+        return a.w > b.w;
+    }
+};
+
+vector<ball> balls;
+
+int dy[256];
+int dx[256];
+const int MAX_TIMEOUT = 4000;
+int main() {
+
+    dy['U'] = 1;dx['U'] = 0;
+    dy['D'] = -1; dx['D'] = 0;
+    dy['L'] = 0; dx['L'] = -1;
+    dy['R'] = 0; dx['R'] = 1;
+
+    cin >> T;
+
+    for (int testcase = 0; testcase < T; testcase++)
+    {
+        balls.clear();
+
+        cin >> N;
+        //ball input
+        for (int num = 1;num <= N;num++)
+        {
+            int x, y, w;
+            char d;
+            cin >> x >> y >> w >> d;
+            y *= 2;
+            x *= 2;
+            balls.push_back({ num, x, y, w, d });
+        }
+
+        int time = 0;
+        int last_crushed_time = 0;
+        while (time < MAX_TIMEOUT)
+        {
+            time++;
+
+            //move balls
+            vector<ball> tmp;
+            for (int idx = 0;idx < balls.size(); idx++)
+            {
+                ball cur = balls[idx];
+                int ny = cur.y + dy[cur.d];
+                int nx = cur.x + dx[cur.d];
+
+                if (nx < -2000 || nx > 2000 ||
+                    ny < -2000 || ny > 2000)
+                    continue;
+
+                tmp.push_back({ cur.num, nx, ny,cur.w, cur.d });
+            }
+
+            //sort tmp
+            sort(tmp.begin(), tmp.end(), Compare_pos());
+
+            // move balls
+            balls.clear();
+            for (int idx = 0; idx < tmp.size();idx++)
+            {
+                int check_idx = idx;
+                int winner_idx = idx;
+                int winner_w = tmp[idx].w;
+                int winner_num = tmp[idx].num;
+
+                // if crushed 
+                while (check_idx + 1 < tmp.size() &&
+                    tmp[check_idx].y == tmp[check_idx + 1].y &&
+                    tmp[check_idx].x == tmp[check_idx + 1].x)
+                {
+                    if (tmp[winner_idx].w < tmp[check_idx + 1].w)
+                    {
+                        winner_idx = check_idx + 1;
+                        winner_w = tmp[check_idx + 1].w;
+                        winner_num = tmp[check_idx + 1].num;
+                    }
+                    else if (tmp[winner_idx].w == tmp[check_idx + 1].w)
+                    {
+                        if (tmp[winner_idx].num < tmp[check_idx + 1].num)
+                        {
+                            winner_idx = check_idx + 1;
+                            winner_w = tmp[check_idx + 1].w;
+                            winner_num = tmp[check_idx + 1].num;
+                        }
+                    }
+                    check_idx++;
+                } //while() same pos check
+
+                balls.push_back(tmp[winner_idx]);
+
+                if (idx != check_idx)
+                {
+                    idx = check_idx;
+                    last_crushed_time = time;
+                }
+            }
+        }
+        if (last_crushed_time == 0)
+            cout << "-1" << endl;
+        else
+            cout << last_crushed_time << endl;
+    }
+
+    return 0;
+}
