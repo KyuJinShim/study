dd80cfef024ae4560c61379795795716f6dd0d0b
KyuJinShim <tlarbwls95@naver.com>
2026-08-13 16:49:38 +0900

    [Passed] 거꾸로 순열 (32ms, 1MB)

---
+++ trail4/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+vector<int> v;
+bool visited[9] = {0,};
+
+void solve(int depth)
+{
+    if(depth==N)
+    {
+        for(int i=0;i<v.size();i++)
+        {
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+        return;
+    }
+    for(int i=N;i>=1;i--)
+    {
+        if(!visited[i])
+        {
+            visited[i] = true;
+            v.push_back({i});
+            solve(depth+1);
+            visited[i] = false;
+            v.pop_back();
+        }
+    }
+}
+
+int main() {
+    cin >> N;
+
+    solve(0);
+
+    return 0;
+}
